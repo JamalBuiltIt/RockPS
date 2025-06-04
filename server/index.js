58cd043ea8d1b5752e1b35bb9be7b7aeaa11adc8
Mall <124288686+JamalBuiltIt@users.noreply.github.com>
2025-06-03 21:28:44 -0400

    env fix

--- server/index.js
+++ server/index.js
@@ -171,7 +171,7 @@ io.on('connection', (socket) => {
     delete playerProfiles[socket.id];
   });
 });
-
-server.listen(3001, () => {
-  console.log('🚀 Server running on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
